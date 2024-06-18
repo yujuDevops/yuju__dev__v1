3e8f1b172fa26a3e83d82d5855e214cd19705a81
SchuttValentin <schuttvalentin@gmail.com>
2024-06-18 09:43:12 -0300

    cambios en componenterizacón, carpeta de layout, section y se creo el navbar por completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 
 function App() {
 
   return (
     <>
-     
+     <Navbar></Navbar>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,58 +1,78 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import logo from '/Users/nimbu/yuju-v1/src/assets/image/RUS-bn.png';
+import logo from '/Users/nimbu/yuju-v1/src/assets/image/logoYujuAzul.png';
+import instagram from '/Users/nimbu/yuju-v1/src/assets/image/web_Icono-instagram.png';
+import facebook from '/Users/nimbu/yuju-v1/src/assets/image/web_Icono-facebook.png';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState({ seguros: false, aseguradoras: false });
 
     const toggleMenu = () => {
-      setIsOpen(!isOpen);
+        setIsOpen(!isOpen);
     };
 
-    
-  return (
-    <nav className="navbar">
-    <div className="navbar-logo">
-      <img src={logo} alt="Yuju Logo" />
-    </div>
-    <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
-      <a href="/">Home</a>
-      <div className="dropdown">
-        <button className="dropbtn">Seguros</button>
-        <div className="dropdown-content">
-          <a href="/auto">Auto</a>
-          <a href="/moto">Moto</a>
-          <a href="/bici">Bici</a>
-          <a href="/hogar">Hogar</a>
-        </div>
-      </div>
-      <div className="dropdown">
-        <button className="dropbtn">Aseguradoras</button>
-        <div className="dropdown-content">
-          <a href="/rus">RUS</a>
-          <a href="/experta">Experta</a>
-          <a href="/san-cristobal">San Cristobal</a>
-        </div>
-      </div>
-      <a href="/institucional">Institucional</a>
-      <a href="/siniestros">Siniestros</a>
-      <a href="/club-yuju">Club Yuju</a>
-      <a href="/contacto">Contacto</a>
-      <div className="navbar-social">
-        <a href="https://facebook.com">
-          <i className="fab fa-facebook-f"></i>
-        </a>
-        <a href="https://instagram.com">
-          <i className="fab fa-instagram"></i>
-        </a>
-      </div>
-    </div>
-    <button className="hamburger" onClick={toggleMenu}>
-      <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
-    </button>
-  </nav>
-   );
+    const toggleDropdown = (dropdown) => {
+        setIsDropdownOpen(prevState => ({
+            ...prevState,
+            [dropdown]: !prevState[dropdown]
+        }));
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <a href="#">
+                <img src={logo} alt="Yuju Logo" />
+                </a>
+            </div>
+            <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+                <a href="/">Home</a>
+                <div className="dropdown">
+                    <button className="dropbtn" onClick={() => toggleDropdown('seguros')}>
+                        Seguros 
+                        <i className={`fa-solid ${isDropdownOpen.seguros ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+                    </button>
+                    {isDropdownOpen.seguros && (
+                        <div className="dropdown-content">
+                            <a href="/auto">Auto</a>
+                            <a href="/moto">Moto</a>
+                            <a href="/bici">Bici</a>
+                            <a href="/hogar">Hogar</a>
+                        </div>
+                    )}
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn" onClick={() => toggleDropdown('aseguradoras')}>
+                        Aseguradoras
+                        <i className={`fa-solid ${isDropdownOpen.aseguradoras ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+                    </button>
+                    {isDropdownOpen.aseguradoras && (
+                        <div className="dropdown-content">
+                            <a href="/rus">RUS</a>
+                            <a href="/experta">Experta</a>
+                            <a href="/san-cristobal">San Cristobal</a>
+                        </div>
+                    )}
+                </div>
+                <a href="/institucional">Institucional</a>
+                <a href="/siniestros">Siniestros</a>
+                <a href="/club-yuju">Club Yuju</a>
+                <a href="/contacto">Contacto</a>
+            </div>
+            <div className="navbar-social">
+                    <a href="#">
+                    <img src={instagram} alt="instagram" /></a>
+                    <a href="#">
+                        <img src={facebook} alt="facebook" />
+                    </a>
+                </div>
+            <button className="hamburger" onClick={toggleMenu}>
+                <i className={`fas ${isOpen ? 'fa-times' : 'fa-bars'}`}></i>
+            </button>
+        </nav>
+    );
 };
- 
-export default Navbar
+
+export default Navbar;
