596b39715394d418e301729b5f2d4c26a44abc75
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2024-07-01 15:20:21 -0300

    agregando cambios buttonHeader

--- src/components/buttonHeader/Button.jsx
+++ src/components/buttonHeader/Button.jsx
@@ -1,5 +1,7 @@
 import './Button.css';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 
 export const Button = ({text,url}) => {
   return (
@@ -12,3 +14,8 @@ export const Button = ({text,url}) => {
   )
 }
 
+Button.propTypes = {
+text: PropTypes.string.isRequired,
+url: PropTypes.string.isRequired
+
+}
