1b143c59ea3eebe2a398f14e697e7710ed2e2557
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2024-07-30 10:20:21 -0300

    motopage responsive v1

--- src/pages/motoPage/motoPageComponents/components/BannerContratar.jsx
+++ src/pages/motoPage/motoPageComponents/components/BannerContratar.jsx
@@ -1,4 +1,6 @@
 import banerContratar from "../../../../assets/image/Banner-contratar.jpg"
+import banerMotosCelular from "../../../../assets/image/imgMotos/Banner-contratar-celular.jpg"
+import banerMotosTablet from "../../../../assets/image/imgMotos/Banner-CTA-tablet.jpg"
 import { Button } from "../../../../components/buttonHeader/Button"
 import "./bannerContratar.css"
 
@@ -6,6 +8,9 @@ export const BannerContratar = () => {
   return (
     <div >
 <img className="imgBannerContratar" src={banerContratar} alt="" />
+<img className="imgBannerMotosTablet" src={banerMotosTablet} alt="" />
+<img className="imgBannerMotosCelular" src={banerMotosCelular} alt="" />
+
 <h2 className="h2Banner">Viví tu pasión. <span className="spanBanner">Protegé tu moto y tu estilo de vida.</span> Yuju te muestra las  <br/>mejores opciones de seguro para tu moto. 
 <br/>
 <br/>
--- src/pages/motoPage/motoPageComponents/components/HeaderMotos.jsx
+++ src/pages/motoPage/motoPageComponents/components/HeaderMotos.jsx
@@ -1,5 +1,6 @@
 import fotoMotos from '../../../../assets/image/Header-seguro-moto.png'
 import fotoMotoTablet from '../../../../assets/image/Header-Moto-tablet.jpg'
+import fotoMotosCelular from  '../../../../assets/image/Header-moto-celular.jpg'
 import { Button } from '../../../../components/buttonHeader/Button'
 
 import "../components/HeaderMotos.css"
@@ -10,12 +11,13 @@ export const HeaderMotos = () => {
    <header className='header-motos'>
   <img src={fotoMotos} alt=""  className='imgPorDefectoMotos' />
   <img  src={fotoMotoTablet} alt="" className='imgRemplazoMotos'  />
+  <img  src={fotoMotosCelular} alt="" className='imgFotoCelular'  />
+  
   <div className='container-motos'>
   <h1 className='h1-motos'>SEGURO DE MOTO</h1>
   <h2 className='h2-motos'>Viví una aventura sin límites. Cotizá y contratá  tu seguro online<br/> en un instante.</h2>
   <h3 className='h3-motos'>En Yuju te ofrecemos protección que va a tu ritmo. Asegurá tu moto de <br/> forma fácil, rápida y a un precio justo.</h3>
-    <Button className="ButtonHeaderMotos" text="Cotiza" url="https://wa.me/5491156307246"/>
-
+   <div className='ButtonHeaderMotos'><Button  text="Cotiza" url="https://wa.me/5491156307246" /></div> 
   </div>
 
   </header>
