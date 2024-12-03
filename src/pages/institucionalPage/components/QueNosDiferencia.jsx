f65d4bbe46bc1bfe279dd9f5aa69d4f9c2a6c888
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2024-12-03 11:53:53 -0300

    agregando pagina Intitucional

---
+++ src/pages/institucionalPage/components/QueNosDiferencia.jsx
@@ -0,0 +1,31 @@
+import "./queNosDiferencia.css"
+
+export const QueNosDiferencia = () => {
+    return (
+        <section className="diferencia-container">
+          <h2 className="diferencia-title">¿Qué nos diferencia?</h2>
+          <div className="diferencia-cards">
+            <div className="diferencia-card">
+              <p className="diferencia-text">
+                Nos destacamos por brindarte un <strong>asesoramiento profesional y personalizado</strong>, que te ayude a proteger lo que es importante para vos, ofreciéndote las mejores opciones de seguros según tus necesidades y expectativas.
+              </p>
+            </div>
+            <div className="diferencia-card">
+              <p className="diferencia-text">
+                Además, aceleramos la <strong>innovación</strong> de los procesos, empleando la <strong>tecnología</strong> en todos y cada una de las etapas del ciclo del cliente.
+              </p>
+            </div>
+            <div className="diferencia-card">
+              <p className="diferencia-text">
+                Desde el momento en que ingresás a nuestra web, podés <strong>cotizar y contratar</strong> tu seguro de forma rápida y sencilla, desde tu celular o cualquier dispositivo con conexión a internet.
+              </p>
+            </div>
+            <div className="diferencia-card">
+              <p className="diferencia-text">
+                También te ofrecemos una <strong>atención de calidad</strong>, a través de nuestros canales de comunicación online, donde podés consultar tus dudas, resolver tus problemas y recibir asistencia siempre que lo necesites.
+              </p>
+            </div>
+          </div>
+        </section>
+      );
+}
---
+++ src/pages/institucionalPage/components/index.js
@@ -0,0 +1,4 @@
+export * from "./HeaderInstitucionalPage"
+export * from "./VisionMisionObjetivo"
+export * from "./QueNosDiferencia"
+export * from "./QueTeOfrecemo"
