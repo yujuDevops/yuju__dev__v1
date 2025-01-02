870e027cc76df8727d05c15fada23408beef7e79
SchuttValentin <schuttvalentin@gmail.com>
2025-01-02 14:02:45 -0300

    cambios

--- src/pages/motoPage/motoPageComponents/components/HeaderMotos.jsx
+++ src/pages/motoPage/motoPageComponents/components/HeaderMotos.jsx
@@ -16,8 +16,9 @@ export const HeaderMotos = () => {
   <h1 className='h1-motos'>SEGURO DE MOTO</h1>
   <h2 className='h2-motos'>Viví una aventura sin límites. Cotizá y contratá <br/> tu seguro online en un instante.</h2>
   <h3 className='h3-motos'>En Yuju te ofrecemos protección que va a tu ritmo. Asegurá tu moto de <br/> forma fácil, rápida y a un precio justo.</h3>
-  <button className='buttonMotosHeader'><a href="
-https://wa.me/5491156307246">Cotizá</a></button>
+  <button className='buttonMotosHeader'>
+  <a href="https://wa.me/5491156307246" target="_blank" rel="noopener noreferrer">Cotizá</a>
+  </button>
   </div>
 
   </header>
