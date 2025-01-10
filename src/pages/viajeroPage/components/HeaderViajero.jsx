79ac91f9e72e9df3a943fd70d1c18a329cc1ad9c
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2025-01-10 09:22:23 -0300

    agregando cambios a viajero

--- src/pages/viajeroPage/components/HeaderViajero.jsx
+++ src/pages/viajeroPage/components/HeaderViajero.jsx
@@ -11,7 +11,7 @@ export const HeaderViajero = () => {
     <div className='container-viejero'>
     <h1 className='h1-viejero'>SEGURO DE ASISTENCIA AL VIAJERO</h1>
     <h2 className='h2-viejero'>Creá tu propia historia. Viajá con la seguridad de que estás protegido con Yuju.</h2>
-    <h3 className='h3-viejero'>Tenemos el plan que se adapta a vos y a tu tipo de viaje, con la cobertura más amplia y personalizada</h3>
+    <h3 className='h3-viejero'>Tenemos el plan que se adapta a vos y a tu tipo de viaje, con la cobertura <br/> más amplia y personalizada</h3>
     <button className='buttonviejeroHeader'><a href="https://wa.me/5491156307246" target="_blank">Cotizá</a></button>
     </div>
   
