82a15ab0577d9e9f65a205044d1184ce6304cb84
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2025-01-13 11:45:57 -0300

    agregando cambios a viajero

--- src/pages/notebookPage/data/dataNotebook.js
+++ src/pages/notebookPage/data/dataNotebook.js
@@ -1,27 +1,27 @@
 export const dataNotebook = [
     {
-      numero: 1.,
-      tituloStrong: "legí el plan ",
+      numero: 1,
+      tituloStrong: "Elegí el plan ",
       tituloNormal: "que más te guste",
       isStrongFirst: true,
       imagen: "https://res.cloudinary.com/dkk8nbi3b/image/upload/v1721740508/web_Icono-selecciona_azul_3_n8ekxs.svg"
     },
     {
-      numero: 2.,
+      numero: 2,
       tituloStrong: "Ingresá tus datos ",
       tituloNormal: " personales y método de pago",
       isStrongFirst: true,
       imagen:"https://res.cloudinary.com/dkk8nbi3b/image/upload/v1721740603/web_Icono-pago_azul_2_pp8ygy.svg"
     },
     {
-      numero: 3.,
+      numero: 3,
       tituloStrong: "Ingresá los datos y fotos ",
       tituloNormal: "Ingresá los datos y fotos",
       isStrongFirst: true,
      imagen: "https://res.cloudinary.com/dkk8nbi3b/image/upload/v1721740383/web_Icono-completar_azul_3_k68cnn.svg"
     },
     {
-      numero: 4.,
+      numero: 4,
       tituloStrong: "¡Listo! ",
       tituloNormal: "Vas a recibir por email el certificado de cobertura",
       isStrongFirst: true,
