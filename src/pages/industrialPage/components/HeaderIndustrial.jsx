8f7d4a8cb8a11868549a16c4225899765403c189
Nawe1337 <155550180+Nawe1337@users.noreply.github.com>
2025-01-13 11:37:47 -0300

    agregando cambios a viajero

--- src/pages/industrialPage/components/HeaderIndustrial.jsx
+++ src/pages/industrialPage/components/HeaderIndustrial.jsx
@@ -8,7 +8,7 @@ export const HeaderIndustrial = () => {
   <img  src="https://res.cloudinary.com/dewcgbpvp/image/upload/v1732025479/Header-industria-celular_knvie4.jpg" alt="" className='imgFotoCelular'  />
   
   <div className='container-industria'>
-  <h1 className='h1-industria'>SEGURO DE INTEGRAL DE COMERCIO E INDUSTRIA</h1>
+  <h1 className='h1-industria'>SEGURO INTEGRAL DE COMERCIO <br/>E INDUSTRIA</h1>
   <h2 className='h2-industria'>Respaldo integral para tu actividad comercial.</h2>
   <h3 className='h3-industria'>Protegé tu negocio con coberturas a medida. Elegí entre nuestros planes <br/>flexibles para comercios de todo tipo y concentrate en hacer crecer tu empresa.</h3>
   <button className='buttonIndustriaHeader'><a href="https://wa.me/5491156307246">Cotizá</a></button>
