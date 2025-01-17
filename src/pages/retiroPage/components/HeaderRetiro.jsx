7ffff51065736b9d44391e6f4e7f8ca5eeee86f0
SchuttValentin <schuttvalentin@gmail.com>
2025-01-17 10:42:06 -0300

    resolución de errores

--- src/pages/retiroPage/components/HeaderRetiro.jsx
+++ src/pages/retiroPage/components/HeaderRetiro.jsx
@@ -1,7 +1,7 @@
 import "./HeaderRetiro.css";
 
 const sendMessage = (title) => {
-  const message = `Hola, quisiera información sobre el seguro de  ${title}.`;
+  const message = `Hola, quisiera información sobre el seguro de retiro.`;
     const phoneNumber = "5491156307246"; // Número de WhatsApp
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.location.href = url;
