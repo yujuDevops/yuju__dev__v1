f4986a722c00aafd3f3d2048f13ff5f72cddf621
SchuttValentin <schuttvalentin@gmail.com>
2025-01-27 09:36:20 -0300

    cambios en boton motos

--- src/pages/motoPage/motoPageComponents/components/CardMotos.jsx
+++ src/pages/motoPage/motoPageComponents/components/CardMotos.jsx
@@ -14,7 +14,7 @@ export const CardMotos = () => {
 `;
     const phoneNumber = "5491156307246"; // Número de WhatsApp
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    window.location.href = url;
+    window.open(url, '_blank', 'noopener,noreferrer,width=600,height=800');
   };
 
   const handlePriceUpdate = (price) => {
