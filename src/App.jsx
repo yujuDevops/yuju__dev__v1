b95a4ade49787e58871407a25ca0fee20fd43d0d
SchuttValentin <schuttvalentin@gmail.com>
2025-02-18 14:19:32 -0300

    instalacion de dependencia GA4 GTM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css';
+import React, { useEffect } from "react";
 import ScrollToTopButton from './components/buttonScrollTop/ScrollToTopButton';
 import AppRouter from './router/AppRouter';
 import FacebookPixel from './components/hooks/FacebookPixel';
 import Metricool from './components/hooks/Metricool';
 import WhatsAppButton from './components/hooks/watsAppButtonGlobal/WhatsAppButton';
+import TagManager from "react-gtm-module";
 
-function App() {
+
+  const App = () => {
+
+    useEffect(() => {
+      TagManager.initialize({ gtmId: "GTM-N6L4XDZD" }); // Reemplaza con tu ID de GTM
+    }, []);
+
 
   return (
     <>
